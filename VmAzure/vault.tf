199c0da20f43dd6aaa92b38844807094c3054763
HamdiNOURI <hamdi.nouri@esprit.tn>
2025-04-28 12:58:54 +0100

    correction of the vault

--- VmAzure/vault.tf
+++ VmAzure/vault.tf
@@ -1,4 +1,8 @@
-data "vault_kv_secret_v2" "admin_creds" {
+/*data "vault_kv_secret_v2" "admin_creds" {
   mount = "secret"
   name  = "AzureWin/admin"
+} */
+data "vault_kv_secret_v2" "azure" {
+  mount = "secret"
+  name  = "azure/creds"
 }
